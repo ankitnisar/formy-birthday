e4c9e420b7381cbd09870253b294f084c001d7bf
Ankit Nisar <nisar92ankit@gmail.com>
2026-02-25 20:21:11 +0530

    committing the changes for the wwebsite

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+})
